69c977059db7f3e6b8672a93b96d788fa594641c
felix <flyok666@126.com>
2017-11-13 11:13:45 +0800

    发送短信验证码

--- frontend/controllers/MemberController.php
+++ frontend/controllers/MemberController.php
@@ -3,6 +3,7 @@
 namespace frontend\controllers;
 
 use common\models\LoginForm;
+use frontend\components\Sms;
 
 class MemberController extends \yii\web\Controller
 {
@@ -36,4 +37,67 @@ class MemberController extends \yii\web\Controller
         return $this->render('index');
     }
 
+    //用户注册
+    public function actionRegister(){
+
+        return $this->render('register');
+    }
+
+    //AJAX发送短信  后台AJAX发送短信功能:
+    public function actionAjaxSms($phone){
+        //接收请求手机号码
+        $phone = '123';
+        //发送短信
+        $response = Sms::sendSms(
+            "石头故事", // 短信签名
+            "SMS_109545450", // 短信模板编号
+            $phone, // 短信接收者
+            Array(  // 短信模板中字段的值
+                "code"=>rand(1000,9999),
+                //"product"=>"dsd"
+            )//,
+        //"123"   // 流水号,选填
+        );
+        //根据$response结果判断是否发送成功 $response->Code
+        //保存验证码(SESSION或)REDIS
+        $redis = new \Redis();
+        $redis->connect('127.0.0.1');
+        $redis->set('captcha_'.$phone,rand(1000,9999),10*60);
+        //验证验证码
+        //$code = $redis->get('captcha_'.$phone);
+        //if($code == '1234');
+
+
+        return 'sucess';// 'fail'
+
+    }
+    //AJAX验证短信
+    public function actionCheckSms($sms){
+        //从redis获取验证码
+        //返回对比结果
+        //验证验证码
+        //$code = $redis->get('captcha_'.$phone);
+        //if($code == '1234');
+    }
+
+
+    //测试阿里大于短信发送功能
+    public function actionSms(){
+        //$sms = new Sms();
+        $response = Sms::sendSms(
+            "石头故事", // 短信签名
+            "SMS_109545450", // 短信模板编号
+            "18080011549", // 短信接收者
+            Array(  // 短信模板中字段的值
+                "code"=>rand(1000,9999),
+                //"product"=>"dsd"
+            )//,
+            //"123"   // 流水号,选填
+        );
+        echo "发送短信(sendSms)接口返回的结果:\n";
+        print_r($response);
+
+        //frontend\components\Sms ---> require '@frontend\components\Sms.php';
+        //Aliyun\Core\Config;   ---> require  '@Aliyun\Core\Config.php';
+    }
 }
